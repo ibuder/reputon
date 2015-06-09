afb73d51a97ab2ebe78a015e211a73a693409479
Immanuel Buder <ibuder@gmail.com>
2015-06-09 12:14:57 -0700

    Add error handling in web output Parse input URL and get listing

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template, request
 from flask_app import app
 
+from get_airbnb_data import make_airbnb_json_dataframe, airbnb_url_to_id
+from get_airbnb_data import get_airbnb_by_id
+
 
 @app.route('/')
 @app.route('/input')
@@ -10,4 +13,13 @@ def input_():
 
 @app.route('/output')
 def output():
+    url = request.args.get('URL')
+    id_ = airbnb_url_to_id(url)
+    if not id_:
+        return render_template("output_inherited.html",
+                               error='Cannot find Airbnb listing id in URL')
+    data = make_airbnb_json_dataframe(get_airbnb_by_id(id_))
+    if data is None:
+        return render_template("output_inherited.html",
+                               error='No data for listing ' + id_)
     return render_template("output_inherited.html")
